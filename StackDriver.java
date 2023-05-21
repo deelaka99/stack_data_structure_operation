c07f6a1e7edafbdef8768227db46d6b643652593
deelaka99 <123wejith@gmail.com>
2023-05-21 09:45:51 +0530

    initialized the stack

---
+++ StackDriver.java
@@ -0,0 +1,15 @@
+public class StackDriver{
+    //initializing the stack array
+    int[] stack = new int[5];
+    //initializing the variable top to -1
+    int top = -1;
+
+    public static void main(String[] args){
+        //System.out.println("Hello testing");
+    }
+
+    //push method
+    public void push(){
+        
+    }
+}
